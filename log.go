69b6230f3cda4cb22f0a6d4b36473340c9cc8ba9
hirrrooo <99264185+hirrrooo@users.noreply.github.com>
2026-06-11 03:25:26 +0000

    feat: add structured logging

---
+++ log.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+func setupLogging(debug bool) {
+	level := slog.LevelInfo
+	if debug {
+		level = slog.LevelDebug
+	}
+	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+		Level: level,
+	})
+	slog.SetDefault(slog.New(handler))
+}
